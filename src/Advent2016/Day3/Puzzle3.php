a7d9c956f31ba745d011e5617ffba6a0ad4aee60
bstumbo <bstumbo1@gmail.com>
2017-01-22 13:37:12 -0600

    Day 3 Part 2 answer
    Signed-off-by: bstumbo <bstumbo1@gmail.com>

--- src/Advent2016/Day3/Puzzle3.php
+++ src/Advent2016/Day3/Puzzle3.php
@@ -21,6 +21,32 @@ class Puzzle3
         }
         
         /**
+         * Transforming string to array for part 2 of question
+         * After filtering and chunking, must create new arrays from the three array "columns"
+         * Re-chunk columns into groups of three to process data vertically
+         */
+        
+        public function TransformInput2 ($input) {
+            $inputarray = [];
+            $inputarray = explode(" ", $input);
+            $filtered = array_filter($inputarray, function ($value) {return $value !== '';});
+            $chunked = array_chunk($filtered, 3);
+            $columnone = array_column($chunked, 0);
+            $columntwo = array_column($chunked, 1);
+            $columnthree = array_column($chunked, 2);
+            
+            $group1 = array_chunk($columnone, 3);
+            $group2 = array_chunk ($columntwo, 3);
+            $group3 = array_chunk($columnthree, 3);
+            
+            $finalarray = array($group1, $group2, $group3);
+            
+            
+            return $finalarray;
+        
+        }
+        
+        /**
          * For each group of three check to see if additiona of any two sides is less than third side
          * Count all triangles that have appropriate side measurements and return answer
          */
@@ -46,5 +72,36 @@ class Puzzle3
         }
          return $triangles;
     }
+    
+    /**
+         * Finding true triangles for question part 2
+         * Same concept, but have to loop through extra array layer
+         */
+    public function TrueTriangle2($input) {
+        
+        $chunked = $this->TransformInput2($input);
+        $triangles = 0;
+        $nope = 0;
+    
+    foreach ($chunked as $e){
+        $chunk = $e;  
+        foreach ($chunk as $i) {
+            $a = $i[0];
+            $b = $i[1];
+            $c = $i[2];
+           
+           if ( $a + $b <= $c || $a + $c <= $b ||  $b + $c <= $a) {
+                    ++$nope;
+           } else {
+            
+            ++$triangles;
+        
+           }
+        }
+         
+    }
+    
+    return $triangles;
+}
     
 }
