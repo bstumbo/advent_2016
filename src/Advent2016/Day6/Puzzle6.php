c4c5030f9525e9724a60448f9cd5d91de375ee11
bstumbo <bstumbo1@gmail.com>
2017-02-09 22:36:21 -0600

    Day 6 part 2 answer
    Signed-off-by: bstumbo <bstumbo1@gmail.com>

--- src/Advent2016/Day6/Puzzle6.php
+++ src/Advent2016/Day6/Puzzle6.php
@@ -33,5 +33,34 @@ class Puzzle6
         
         return $answer ;
     }
+    
+    public function repeatCode2($input) {
+        $chunkarray = [];
+        $columnarray = [];
+        $answer = [];
+        $inputarray = explode(PHP_EOL, $input); //explode $input into array
+    
+        foreach ($inputarray as $key => $value){ //split each line into new array and remove blanks
+            $split = str_split($value);
+            $filtered = array_filter($split, function ($value1) {return $value1 !== '';});
+            $chunkarray[] = $filtered;
+        }
+        
+        $counter = 0;
+        while ($counter < 8) { //Create another array from columns of previous array
+            $column = array_column($chunkarray, $counter);
+            $columnarray[] = $column;
+            $counter++; //Add 1 to counter
+        }
+        
+        foreach ($columnarray as $column) { //Count values in final array and add highest number to answer array
+            $count = array_count_values($column);
+            arsort($count);
+            end($count);
+            $answer[] = key($count);
+        }
+        
+        return $answer ;
+    }
 
 }
