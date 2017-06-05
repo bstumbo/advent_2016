6b374707504e6b2410e9eb4ded1da89dc3f20d8e
bstumbo <bstumbo1@gmail.com>
2017-06-05 10:21:48 -0500

    Reworked application and broken everything out into smaller functions
    Nearly solved.
    
    Signed-off-by: bstumbo <bstumbo1@gmail.com>

--- src/Advent2016/Day9/Puzzle9.php
+++ src/Advent2016/Day9/Puzzle9.php
@@ -19,51 +19,52 @@ class Puzzle9
         $arraySplit = [];
         $arrayOriginal = [];
         foreach ($individual as $line) {
-            $arrayOriginal[] = $line;
-            $arraySplit[] = str_split(trim($line));
+            $arrayOriginal[] = preg_replace('/\s+/', '', $line);
+            $arraySplit[] = str_split(preg_replace('/\s+/', '',$line));
         }
         
         return array($arrayOriginal, $arraySplit);
     }
     
     /**
-     * 
+     * Separate out markers (XxY) from rest of string in array
+     *
+     * To be used to count number of characters needed and them multiple them
      */
     
-    function extractMarker($input) {
+    function separateXY($original) {
+        //$string = $original[0];
+        preg_match_all('/([\d]+)/', $original, $preg);
+        $markarray = array_chunk($preg[0], 2);
+            
+        return $markarray;
     
-            $start = $this->inputArray($input);
-            $original = $start[0];
-            $check = $start[0];   //Keep original array strings in tact; To be used as check later;
-            $new = [];
-            $finalcount = [];
+    }
+    
+    /**
+    * In each array, get values of $x (first integer) and $y (second integer)
+    *
+    * Get position of string to be repeated ($repvalue) and repeat it $y times ($insert)
+    *
+    * Set new value to be inserted into $original array ($updated)
+    *
+    * 
+    */
+    
+    function createNewString ($markarray, $original, $index){
+        
+        $repcount = 0;
+        $i = 0;
+        $buildstring = [];
+        $newmark = [];
+        
+        foreach($markarray as $key => $value){
             
-            foreach ($original as $index => $string){
-              //  $new[] = preg_split('/\(|\)/', $value); // Separate out markers (XxY) from rest of string in array
-             $flag = 0; 
-             preg_match_all('/([\d]+)/', $string, $preg);
-             $markarray = array_chunk($preg[0], 2);
-             
-             
-            /**
-             * In each array, get values of $x (first integer) and $y (second integer)
-             *
-             * Get position of string to be repeated ($repvalue) and repeate it $y times ($insert)
-             *
-             * Set new value to be inserted into $original array ($updated)
-             *
-             * 
-             */
-            
-            $repcount = 0;
-            $i = 0;
-            
-          foreach($markarray as $key => $value){
-                  if ($i < $repcount){
+            if ($i < $repcount){
                          $i++;
                          continue;
                    } 
-    
+                
                 $x = $value[0];
                 $y = $value[1];
                 
@@ -83,25 +84,75 @@ class Puzzle9
                 $repcount = substr_count($repvalue, '(');    //Counting the number of "fake" marks in the repeating string
                 $i = 0;
                 /*
-                 * Get updated $orginal array string.  Then up date the actual array
+                 * Get updated $orginal array string.  Then update the actual array
                  */
-                $upfinal= substr_replace($original[$index], $insert, $positionstart, $endposition);
                 
-                $new[] = $insert;
-               
-   
-               // $original[$index] = $upfinal;
-            
-            } 
-            $finalstring = '';
-            foreach ($new as $key => $final) {
-                $finalstring .= $final;
+                $buildstring[] = $insert;
+                $newmark[] = "(" . $x . "x" . $y . ")";
+                
+                $upfinal = substr_replace($original[$index], '', $positionstart, $endposition);
+                
+                $original = array($upfinal);
+                
+                            
             }
             
-            return $new;
+        return array($newmark, $buildstring);
+    }
+     
+    function buildFinalString($finalarray, $original) {
+        //$original = $original[0];
+        $marks = $finalarray[0];
+        $strings = $finalarray[1];
         
-      }
+        foreach ($marks as $index => $string) {
+            $place = strpos($original, $string);
+            $xvalue = substr($string, 1, 1);
+            $count = strlen($string) + intval($xvalue);
+            $original = substr_replace($original, $strings[$index], $place, $count);
+        }
+        
+        return $original;
+    }
+    
+    function extractMarker($input) {
+        
+            $finalstring = [];
+    
+            $start = $this->inputArray($input);
+            $original = $start[0];
+            
+            foreach ($original as $index => $string){
+                    
+                    //Separate XxY to figure out what charaters need to be duplicated
+                    
+                    $markarray = $this->separateXY($string);
+                    
+                    //Create new string following XxY guidelines
+                    
+                    $finalarray = $this->createNewString($markarray, $original, $index);
+                    
+                    $finalstring[] = $this->buildFinalString($finalarray, $string);    
+        
+            } 
+        
+        return $finalstring;
     
     }
+    
+    function decompressedLength($input) {
+        $finalstring = $this->extractMarker($input);
+        $answer = [];
+        foreach ($finalstring as $final) {
+            $strippedfinal = preg_replace('/\s+/', '', $final); 
+            $answer[] = strlen($strippedfinal);
+        }
+        
+        $decompressed = array_sum($answer);
+        
+    
+        return $finalstring;
+    }
+
 
 }
--- tests/Advent2016/Tests/Day9/Puzzle9Test.php
+++ tests/Advent2016/Tests/Day9/Puzzle9Test.php
@@ -14,10 +14,12 @@ class Day8Test extends \PHPUnit_Framework_TestCase
     
     public function testcheckInput() {
      
-    $input = '(6x1)(1x3)ABD(2x3)AB';
+    
+    $input = '(6x1)(1x3)ABC(2x3)(1x3)A(1x5)BC';
+
     
     $puzzle = new Puzzle9();
-    $result = $puzzle->extractMarker($input);
+    $result = $puzzle->decompressedLength($input);
     
      print_r($result);
     }
